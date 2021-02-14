55f52ab1c0b97a2d0ed722917beaf4a802ad8b42
netooo <kazushi.mitsue@gmail.com>
2021-02-14 23:24:10 +0900

    Add: save and load

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"github.com/netooo/sample-go/controllers"
+	"fmt"
+	"io/ioutil"
 )
 
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
 func main() {
-	controllers.StartWebServer()
+	p1 := &Page{Title: "text", Body: []byte("This is a sample Page.")}
+	p1.save()
+
+	p2, _ := loadPage(p1.Title)
+	fmt.Println(string(p2.Body))
 }
